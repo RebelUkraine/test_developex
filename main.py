29cf8b9a21b7c94c20c38625699375a3f212c16a
RebelUkraine <support4uf1@gmail.com>
2019-12-16 14:29:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from sys import argv
 param = argv[1]
 api_get = requests.get('https://api.domainsdb.info/v1/domains/search?' , params={'domain' : param})
 api_answer = api_get.json()
-print(api_answer['domains'][0]['NS'])
+print(api_answer['domains'][0]['A'])
